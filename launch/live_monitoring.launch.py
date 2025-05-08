1d4d66d70b16c265c500f558e502ab52ddda2c50
Luc <luc001@e.ntu.edu.sg>
2025-05-08 13:54:57 +0000

    Minor change

--- launch/live_monitoring.launch.py
+++ launch/live_monitoring.launch.py
@@ -24,6 +24,17 @@ def generate_launch_description():
         ), launch_arguments={'params_file': os.path.join(get_package_share_directory('monitoring'), 'config', 'mqtt_live_monitoring.yaml')}.items()
     )
 
+    # Read the yaml file and broadcast all the static transforms: leader -> agentX
+    formation_shape_broadcaster = Node(
+        name='formation_shape_broadcaster',
+        executable='formation_shape_broadcaster',
+        package='formation_controller',
+        parameters=[{
+            'yaml_file_path': os.path.join(get_package_share_directory('formation_controller'), 'config', 'formation_shape.yaml'),
+            'leader_frame': 'usv1/gps_link',
+        }]
+    )
+
     usv1_tf = Node(
         name='usv1_tf',
         executable='gps_heading_to_tf',
@@ -85,5 +96,6 @@ def generate_launch_description():
         usv2_tf,
         usv1_path_plotter,
         usv2_path_plotter,
+        formation_shape_broadcaster,
         TimerAction(period=4.0, actions=[rosbag_record]),
     ])
